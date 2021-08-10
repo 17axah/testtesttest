c540de9fcc396e5baedad1873449e726f990fa33
17axaH <17axaH@mail.ru>
2021-08-10 16:40:41 +0000

    Update 2021-08-10T16:40:37.880Z

--- tasks/replace.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const replace = require('gulp-replace');
-
-module.exports = function () {
-  return gulp.src('dest/**/*')
-    .pipe(replace('"/', `"${process.env.BASE_URL}`))
-    .pipe(gulp.dest('dest/'));
-}
